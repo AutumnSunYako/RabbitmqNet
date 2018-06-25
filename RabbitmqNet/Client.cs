72c410ba2a1004a3e23b7ab756b4496f3cf5f7fb
姜秋阳 <jiangqiuyang@DESKTOP-V0J50VF>
2018-06-25 10:41:17 +0800

    first commit

---
+++ RabbitmqNet/Client.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Framing.Impl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitmqNet
+{
+    public class Client
+    {
+        public Connection Connection { get; }
+
+        public Client(Connection connection)
+        {
+            Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <typeparam name="TIn"></typeparam>
+        /// <param name="routingKey"></param>
+        /// <param name="value"></param>
+        public void Publish<TIn>(string routingKey, TIn value)
+        {
+            if (!Connection.IsConnected)
+            {
+                Connection.TryConnect();
+            }
+            //using (var channel = Connection.CreateModel())
+            //{
+            var channel = Connection.channel;
+            channel.ExchangeDeclare(exchange: Connection.Exchange,
+                                    type: "topic");
+
+            var message = JsonConvert.SerializeObject(value);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: Connection.Exchange,
+                               routingKey: routingKey,
+                               basicProperties: null,
+                               body: body);
+            //}
+        }
+        /// <summary>
+        /// 订阅
+        /// </summary>
+        /// <typeparam name="TIn"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="bindingKey"></param>
+        /// <param name="isReply"></param>
+        public void Subscribe<TIn, TOut>(string bindingKey, bool isReply = true) where TIn : IRabbitMQNetHandler<TOut>
+        {
+            if (!Connection.IsConnected)
+            {
+                Connection.TryConnect();
+            }
+            //using (var channel = Connection.CreateModel())
+            //{
+            var channel = Connection.channel;
+            channel.ExchangeDeclare(exchange: Connection.Exchange, type: "topic");
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: Connection.Exchange,
+                              routingKey: bindingKey);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var handler = Activator.CreateInstance<TIn>();
+                var message = Encoding.UTF8.GetString(ea.Body);
+                handler.Handle(JsonConvert.DeserializeObject<TOut>(message));
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+            //}
+        }
+    }
+}
---
+++ RabbitmqNet/Connection.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitmqNet
+{
+    public class Connection
+    {
+        /// <summary>
+        /// rabbitmq连接字串：amqp://guest:guest@192.168.0.252:5672/
+        /// </summary>
+        public string ConnectionString { private set; get; }
+        public string Exchange { get; set; }
+        public IConnection conn { get; set; }
+        public IModel channel { get; set; }
+        public EventingBasicConsumer Consumer { get; set; }
+        public bool IsConnected
+        {
+            get
+            {
+                return conn != null && conn.IsOpen;
+            }
+        }
+
+        public Connection(string connectionString, string exchange)
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            Exchange = exchange ?? throw new ArgumentNullException(nameof(exchange));
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri(connectionString);
+            conn = factory.CreateConnection();
+            channel = CreateModel();
+        }
+        public void Close()
+        {
+            channel.Close();
+            conn.Close();
+        }
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMQ connections are available to perform this action");
+            }
+
+            return conn.CreateModel();
+        }
+        public void TryConnect()
+        {
+            if (!IsConnected)
+            {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.Uri = new Uri(ConnectionString);
+                conn = factory.CreateConnection();
+                channel = CreateModel();
+            }
+        }
+    }
+}
